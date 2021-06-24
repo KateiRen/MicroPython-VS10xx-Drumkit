2e4d409b3e8131dd8f77e14b4340e4529c625f8a
KateiRen <Karsten.Hartlieb@gmx.de>
2021-06-24 22:04:46 +0200

    implement new class

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # MicroPython VS10xx Drumkit
 
-A minimalistic test / approach to try operating a VS10xx module in Live MIDI modus.
+A minimalistic test / approach to try operating a VS10xx module in real time MIDI mode.
+An example for Arduino can be found [here](https://gist.github.com/microtherion/2636608).
 
 Based on my VS10xx port [here](https://github.com/KateiRen/vs10xx-micropython), methods to activate MIDI and send notes need to be added.
--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 import vs10xx
 from machine import SPI
 
+
+vs10xx_MIDI_Plugin = [0x0007, 0x0001, 0x8050, 0x0006, 0x0014, 0x0030, 0x0715, 0xb080,
+                    0x3400, 0x0007, 0x9255, 0x3d00, 0x0024, 0x0030, 0x0295, 0x6890,
+                    0x3400, 0x0030, 0x0495, 0x3d00, 0x0024, 0x2908, 0x4d40, 0x0030,
+                    0x0200, 0x000a, 0x0001, 0x0050]
+
+
+class VS10XXMidi(vs10xx.Player):
+    def __init__(self, spi, xResetPin, dReqPin, xDCSPin, xCSPin, CSPin = None):
+        super().__init__(spi, xResetPin, dReqPin, xDCSPin, xCSPin, CSPin)
+        self.__setupMIDI__()
+
+    def __setupMIDI__(self):
+        i = 0
+        while i < len(vs10xx_MIDI_Plugin):
+            addr = vs10xx_MIDI_Plugin[i]
+            n = vs10xx_MIDI_Plugin[i+1]
+            i+=1
+            while n:
+                val = vs10xx_MIDI_Plugin[i+2]
+                super().writeRegister(addr, val)
+                print("Writing {value} to the address {address}".format(value=val, address=addr))
+                i+=3
+                n-=1
+
+
 spi = SPI(1, vs10xx.SPI_BAUDRATE) # SPI bus id=1 pinout: SCK = 14, MOSI = 13, MISO = 12
 
-player = vs10xx.Player(
+midiplayer = VS10XXMidi(
     spi,
     xResetPin = 21,
     dReqPin = 22,
@@ -12,6 +38,36 @@ player = vs10xx.Player(
     CSPin = None
 )
 
-print("VS10xx Player set up.")
-player.setVolume(0.8) # the range is 0 to 1.0
+# print("VS10xx Player set up.")
+# player.setVolume(0.8) # the range is 0 to 1.0
 
+
+#VSLoadUserCode();
+ 
+
+# void VSLoadUserCode(void) {
+#   int i = 0;
+
+#   while (i<sizeof(sVS1053b_Realtime_MIDI_Plugin)/sizeof(sVS1053b_Realtime_MIDI_Plugin[0])) {
+#     unsigned short addr, n, val;
+#     addr = sVS1053b_Realtime_MIDI_Plugin[i++];
+#     n = sVS1053b_Realtime_MIDI_Plugin[i++];
+#     while (n--) {
+#       val = sVS1053b_Realtime_MIDI_Plugin[i++];
+#       VSWriteRegister(addr, val >> 8, val & 0xFF);
+#     }
+#   }
+# }                                
+
+# void VSWriteRegister(unsigned char addressbyte, unsigned char highbyte, unsigned char lowbyte){
+#   while(!digitalRead(VS_DREQ)) ; //Wait for DREQ to go high indicating IC is available
+#   digitalWrite(VS_XCS, LOW); //Select control
+
+#   //SCI consists of instruction byte, address byte, and 16-bit data word.
+#   SPI.transfer(0x02); //Write instruction
+#   SPI.transfer(addressbyte);
+#   SPI.transfer(highbyte);
+#   SPI.transfer(lowbyte);
+#   while(!digitalRead(VS_DREQ)) ; //Wait for DREQ to go high indicating command is complete
+#   digitalWrite(VS_XCS, HIGH); //Deselect Control
+# }
